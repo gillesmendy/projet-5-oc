7b09dc52674dc621af1533b3692e86a13aa87a49
gillesmendy <165325415+gillesmendy@users.noreply.github.com>
2024-08-04 23:55:41 +0200

    feature: caroussel added with javascript updated

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,38 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+let currentIndex = 0;
+const bannerImage = document.getElementById('bannerImage');
+const tagline = document.getElementById('tagline');
+
+// Mise a jour images caroussel
+function updateCarousel(index) {
+    bannerImage.src = `./assets/images/slideshow/${slides[index].image}`;
+    tagline.innerHTML = slides[index].tagLine;
+}
+
+// Evenements et fleches
+document.getElementById('previous').addEventListener('click', () => {
+	if (currentIndex != 0) 
+    currentIndex = currentIndex - 1 
+	else
+	currentIndex = slides.length - 1
+    updateCarousel(currentIndex);
+});
+
+document.getElementById('next').addEventListener('click', () => {
+    if (currentIndex < slides.length) 
+	currentIndex = currentIndex + 1 
+	else
+	currentIndex = 0
+    updateCarousel(currentIndex);
+});
+
+// Initialisation du carrousel
+updateCarousel(currentIndex);
+
+//fct pour creer tous les points pour etat initial, point remplit = slide affiche, 
+//ajouter une classe pour point actif, creer point + id sur les points
+//utiliser current index logique
